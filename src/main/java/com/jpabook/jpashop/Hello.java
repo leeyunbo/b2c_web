b915cf9f742de02fa5716e255819a10e7857b4e6
leeyunbo <servers1@naver.com>
2021-01-25 22:27:02 +0900

    [개발] lombok 테스트 코드 작성

---
+++ src/main/java/com/jpabook/jpashop/Hello.java
@@ -0,0 +1,16 @@
+package com.jpabook.jpashop;
+
+/**
+ * lombok 테스트 코드
+ * @author yunbok
+ */
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+
+    private String data;
+
+}
