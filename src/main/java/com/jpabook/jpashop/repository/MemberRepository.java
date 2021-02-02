7f624cde7754bbfba75e3570b20b4779d665da01
leeyunbo <servers1@naver.com>
2021-02-02 22:28:55 +0900

    [개발] MemberService 개발

--- src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -1,18 +1,26 @@
 package com.jpabook.jpashop.repository;
 
 import com.jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+/**
+ * @author yunbok
+ * 회원 조회 및 등록 레포지토리
+ */
+
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    // Spring Boot가 만들어 놓은 JPA의 엔티티 매니저를 주입하라는 에노테이션
-    @PersistenceContext
-    private EntityManager em;
+    // Spring Boot가 만들어 놓은 JPA의 엔티티 매니저를 주입하라는 에노테이션 @PersistenceContext
+    // 롬북에 의하여 private final로 깔끔하게 대체 가능
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
@@ -23,7 +31,7 @@ public class MemberRepository {
     }
 
     public List<Member> findAll() {
-        // 얘는 엔티티에 대하여 쿼리문을 날리는 것, Member m으로 치환해서 m을 select하라는 의미
+       // JPQL은 엔티티에 대하여 쿼리문을 날리는 것, Member m으로 치환해서 m을 select하라는 의미
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
---
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,84 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author yunbok
+ * 회원 조회 및 등록 서비스
+ */
+
+
+//JPA 관련 로직은 무조건 트랜잭션 내에서 수행되어야함
+@Service
+@Transactional
+
+// 롬복에 의하여 private final 필드에 대해 자동 생성자 주입
+@RequiredArgsConstructor
+public class MemberService {
+
+
+    private final MemberRepository memberRepository;
+
+    // 요즘은 생성자 인젝션이 권장된다.
+    // memberRepository가 실행 중간에 바뀔 일이 없다. (final)
+    // 테스트 케이스에서 memberRepository를 직접 생성할 때 생성자 파라미터에 의해 수동 주입을 놓칠 일이 없다.
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원가
+     * @param member : 회원 엔티티
+     * @return : 회원 번호
+     */
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        // DB에 저장 되기전에 ID가 저장되어있는 것은 무조건 보장됨 (generatedValue)
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     * @param member : 회원 엔티티
+     */
+    // 물론 복수의 WAS를 통해 접근하여 동시에 같은 계정을 생성하는 경우가 발생하면 이 로직이 틀어질 수도 있다.
+    // 그런 경우를 대비하여 디비 필드에 UNIQUE 제약 조건을 걸어주는 것이 권장된다.
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (findMembers.size() != 0) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * @return : 회원 엔티티 리스트
+     */
+    // readOnly 추가하면 데이터를 단순히 읽어올 때 성능 최적화가 이루어짐
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 단일 회원 조회
+     * @param memberId : 회원 엔티티
+     * @return : 회원 엔티티
+     */
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
