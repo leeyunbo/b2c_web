2b8b6f0f8bbaff45ec0496d39af3aab1df14c73d
leeyunbo <servers1@naver.com>
2021-02-04 23:08:02 +0900

    [개발] 주문 관련 서비스 개발

---
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,69 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Delivery;
+import com.jpabook.jpashop.domain.Item.Item;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.repository.ItemRepository;
+import com.jpabook.jpashop.repository.MemberRepository;
+import com.jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 주문 하기
+     * CASCADE 옵션 덕분에 orderRepository.save()만 해주면 됨
+     * CASCADE는 특정 엔티티를 어떤 엔티티가 유일하게 관리할 경우에만 사용해라.
+     * 복수의 엔티티에서 관리하는 경우 CASCADE를 사용하지 마라. 별도의 레포지토리를 생성해라.
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 멤버, 품목 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    // 검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch)
+//    }
+
+}
