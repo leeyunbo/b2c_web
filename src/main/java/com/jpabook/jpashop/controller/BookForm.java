62631b6c400499e136891385ca66b556f69ad36c
leeyunbo <servers1@naver.com>
2021-02-07 19:25:05 +0900

    [개발] 상품 등록 리스트 출력 개발 완료

--- src/main/java/com/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/com/jpabook/jpashop/controller/BookForm.java
@@ -3,6 +3,10 @@ package com.jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NegativeOrZero;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class BookForm {
@@ -10,8 +14,13 @@ public class BookForm {
     //== 상품 공통 속성 ==//
     private Long id;
 
+    @NotEmpty(message = "도서 이름격은 필수 입니다")
     private String name;
+
+    @NegativeOrZero(message = "도서 가격은 0 이상이 되어야 합니다")
     private int price;
+
+    @NegativeOrZero(message = "도서 수량은 0 이상이 되어합야 합니다")
     private int stockQuantity;
 
     //== 책 속성 ==//
--- src/main/java/com/jpabook/jpashop/controller/ItermController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItermController.java
@@ -1,12 +1,17 @@
 package com.jpabook.jpashop.controller;
 
 import com.jpabook.jpashop.domain.Item.Book;
+import com.jpabook.jpashop.domain.Item.Item;
 import com.jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,11 +22,15 @@ public class ItermController {
     @GetMapping("/items/new")
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
-        return "items/createItemForm";
+        return "items/createItemsForm";
     }
 
     @PostMapping("/items/new")
-    public String create(BookForm form) {
+    public String create(@Valid BookForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "items/createItemsForm";
+        }
+
         Book book = new Book();
 
         // 나중에 createBook() 메서드로 통합시켜서 만들어놓자 => 엔티티에 ( Setter 방지 )
@@ -33,5 +42,12 @@ public class ItermController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
