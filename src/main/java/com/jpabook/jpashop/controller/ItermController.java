3443acfc349a38afb4e88d71fc6f62604aac6257
leeyunbo <servers1@naver.com>
2021-02-07 18:46:00 +0900

    [개발] 상품 등록 view 개발 완료

--- src/main/java/com/jpabook/jpashop/controller/ItermController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItermController.java
@@ -1,4 +1,37 @@
 package com.jpabook.jpashop.controller;
 
+import com.jpabook.jpashop.domain.Item.Book;
+import com.jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class ItermController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+
+        // 나중에 createBook() 메서드로 통합시켜서 만들어놓자 => 엔티티에 ( Setter 방지 )
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
 }
