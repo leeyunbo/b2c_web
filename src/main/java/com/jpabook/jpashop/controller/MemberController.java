77fae1432d206de7bcf05c9ba7c5c921f042631d
leeyunbo <servers1@naver.com>
2021-02-07 18:03:56 +0900

    [개발] 회원가입 관련 컨트롤러 메서드 개발 완료

--- src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -1,11 +1,18 @@
 package com.jpabook.jpashop.controller;
 
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +28,25 @@ public class MemberController {
         log.info("member controller");
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    // MemberForm을 쓰는 이유? => 단순히 멤버 엔티티를 넘겨주게 되면 맞쳐줘야 하는 쓸데 없는 코드가 많아져 더러워짐
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        // 자동으로 result(에러 관련 클래스)를 끌고감
+        // 끌고가면 해당 페이지에서 오류 관련 태그를 거치게 됨
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
