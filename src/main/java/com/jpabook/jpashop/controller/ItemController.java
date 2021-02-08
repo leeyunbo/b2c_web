f1e4b2b00ded35167f2018320a59d5536d74d21d
leeyunbo <servers1@naver.com>
2021-02-08 21:12:48 +0900

    [개발] updateItemForm.html 추가 및 PostMapping 추가

--- src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -66,5 +67,20 @@ public class ItemController {
 
         model.addAttribute("form", form);
         return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
