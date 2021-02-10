a88e60c77bd53e28eb138c632c77554cf93c9fc0
leeyunbo <servers1@naver.com>
2021-02-11 00:17:04 +0900

    [개발] 검색 기능 및 취소 기능 구현 완료

--- src/main/java/com/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/com/jpabook/jpashop/controller/OrderController.java
@@ -10,10 +10,7 @@ import com.jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,10 +45,20 @@ public class OrderController {
     }
 
     // 단순히 조회하는 기능은 그냥 컨트롤러에서 바로 리포지토리에 접근해도 됨 (본인 선택.. 얽매일 필요는 X)
+    // ModelAttribute -> Model에 자동으로 담김
     @GetMapping("/orders")
     public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model) {
         List<Order> orders = orderService.findOrders(orderSearch);
+
+        model.addAttribute("orders", orders);
         return "order/orderList";
     }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+
 
 }
