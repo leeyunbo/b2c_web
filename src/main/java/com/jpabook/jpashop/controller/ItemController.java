fc8f5f4113dfe88d9ef5d773ebdbfa2c4fb92958
leeyunbo <servers1@naver.com>
2021-03-11 21:37:03 +0900

    [개발] 요청 게시판 상품 등록 기능 개발 완료

--- src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -26,8 +23,14 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("/items/new")
-    public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
+    public String createForm(Model model, @RequestParam(name = "itemName", required = false) String itemName) {
+        BookForm form = new BookForm();
+
+        if(itemName != null) {
+            form.setName(itemName);
+        }
+
+        model.addAttribute("form", form);
         return "items/createItemsForm";
     }
 
